ebcf732eaf072952206997bd280ee0fdd6d98dcd
Shatakshi Rajput <shatakshirajput02@gmail.com>
2025-05-03 14:49:56 +0530

    Updated UI

--- init/index.js
+++ init/index.js
@@ -15,7 +15,7 @@ async function main() {
 const initDB = async()=>{
     await Listing.deleteMany({});
     initData.data= initData.data.map((obj)=>({
-        ...obj,owner:"6803a44874204d54ca00b567"
+        ...obj,owner:"6815b4b6030cdeb381facd11"
     }));
     await Listing.insertMany(initData.data);
     console.log("data was initialised");
